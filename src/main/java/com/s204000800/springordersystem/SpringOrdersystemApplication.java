d6b638b2068866b3ca1d070449724e0d010858d5
s20400080 <s20400080@gms.ndhu.edu.tw>
2022-10-23 22:03:43 +0800

    spring-ordersystem -- 初始化project

---
+++ src/main/java/com/s204000800/springordersystem/SpringOrdersystemApplication.java
@@ -0,0 +1,13 @@
+package com.s204000800.springordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOrdersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/s204000800/springordersystem/SpringOrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s204000800.springordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrdersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
